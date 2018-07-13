d38d580ea647c6c62b14ff8cf4bf2513ff8e2a6c
iobreese <iobreese@gmail.com>
2018-07-13 23:23:07 +0000

    Added README.

--- code/raw_code/0-download-data.R
+++ code/raw_code/0-download-data.R
@@ -7,3 +7,7 @@ txt = readtext(here("data/raw_data/posts/")) #had to move posts up to raw_data
 library(dplyr)
 glimpse(txt)
 
+#Save as rda file
+data_date = date()
+save(txt,data_date,file=here("data/tidy_data",
+                             "simply_data_before_processing_07-13-2018.rda"))
